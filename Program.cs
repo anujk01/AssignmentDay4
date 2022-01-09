ea46417a0fc38e27e9b8131dad3d0025523b5b3d
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-01-09 22:27:12 +0530

    Day4

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ObjectAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 UC1 = new UC1();
+            //UC1.EmpAttendance();
+            //UC2 UC2 = new UC2();
+            //UC2.EmpDailyWage();
+            //UC3 UC3 = new UC3();
+            //UC3.PartTimeEmpWage();
+            //UC4 UC4 = new UC4();
+            //UC4.UsingSwitchCase();
+            //UC5 UC5 = new UC5();
+            //UC5.CalculateMonthWages();
+            UC6 UC6 = new UC6();
+            UC6.CalculateWageTill100HrsOr20IsReached();
+        }
+    }
+}
